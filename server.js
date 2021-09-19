8e2579834361f12568541879ea2dd36a27a98a9f
PredragD01 <43607990+PredragD01@users.noreply.github.com>
2021-09-19 12:22:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,5 @@
-//Install express server
-const express = require('express');
-const path = require('path');
+const express = require('express')
+const PORT = process.env.PORT || 5000  # Fall back to port 5000 if process.env.PORT is not set
 
-const app = express();
-const allowCrossDomain = function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
-
-  // intercept OPTIONS method
-  if ('OPTIONS' == req.method) {
-    res.send(200);
-  }
-  else {
-    next();
-  }
-};
-// Serve only the static files form the angularapp directory
-app.use(express.static(__dirname + '/angularapp'));
-
-app.get('/*', function(req,res) {
-
-  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
-});
-
-app.configure(function () {
-  app.use(allowCrossDomain);
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(app.router);
-  app.use(express.static(path.join(application_root, "public")));
-  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
-});
-
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
-
+express()
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
