77d3fa4a31df0831e601b8cc461b83dfb59f4798
PredragD01 <43607990+PredragD01@users.noreply.github.com>
2021-09-19 12:19:08 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
-//Install express server    
+//Install express server
 const express = require('express');
+const path = require('path');
 
-const path = require('path');   
+const app = express();
+const allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With');
 
-const app = express();   
+  // intercept OPTIONS method
+  if ('OPTIONS' == req.method) {
+    res.send(200);
+  }
+  else {
+    next();
+  }
+};
+// Serve only the static files form the angularapp directory
+app.use(express.static(__dirname + '/angularapp'));
 
-// Serve only the static files form the dist directory    
-app.use(express.static(__dirname + '/dist'));
+app.get('/*', function(req,res) {
 
-app.get('/*', function(req,res) {  
-    res.sendFile(path.join(__dirname+'/dist/index.html'));   
-});  
+  res.sendFile(path.join(__dirname+'/angularapp/index.html'));
+});
 
-// Start the app by listening on the default Heroku port    
-app.listen(process.env.PORT || 5000);
+app.configure(function () {
+  app.use(allowCrossDomain);
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use(express.static(path.join(application_root, "public")));
+  app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
+
